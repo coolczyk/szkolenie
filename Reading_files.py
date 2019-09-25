d3db364054f13d8f25339371e4a9d49bfc0c17f1
Kulczas <kulczyk.lukasz@gmail.com>
2019-09-25 10:28:41 +0200

    First commit

---
+++ Reading_files.py
@@ -0,0 +1,12 @@
+#employees_var = open("employees.txt", "r")
+#print(employees_var.readable())
+#print(employees_var.readline())
+#print(employees_var.read())
+#employees_var.close()
+
+#5
+employees_var = open("employees.txt", "r")
+for for_var in employees_var.readline():
+    print(for_var)
+
+employees_var.close()
---
+++ Writing_to_files.py
@@ -0,0 +1,10 @@
+employees_var = open("employees.txt", "a")
+employees_var.write("Lukasz - Tower Head")
+employees_var.close()
+
+#2
+employees_var = open("employees.txt", "a")
+employees_var.write("\nKelly - HR\n")
+employees_var.close()
+
+
